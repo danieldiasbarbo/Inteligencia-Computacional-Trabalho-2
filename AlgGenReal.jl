2f232fac1e09b453fd5df1ad140b6c4cf8ddf038
Daniel Dias Barbosa <danieldiasbarbo@gmail.com>
2023-11-11 08:39:11 -0300

    Melhorando prints AlgGen

--- AlgGenReal.jl
+++ AlgGenReal.jl
@@ -3,7 +3,7 @@ using Evolutionary
 
 function AlgGenOptimization(dimension)
     selection_methods = [Evolutionary.rouletteinv]
-    mutation_methods = [Evolutionary.gaussian(), Evolutionary.uniform()]
+    mutation_methods = [(Evolutionary.gaussian(), "gaussiano"), (Evolutionary.uniform(), "uniforme")]
     crossover_methods = [Evolutionary.DC, Evolutionary.AX]
 
     println("ALGORITMO GENETICO DIMENSAO $dimension")
@@ -12,13 +12,13 @@ function AlgGenOptimization(dimension)
         for mut in mutation_methods
             for cros in crossover_methods
                 println("selecao: $sel")
-                println("mutacao: $mut")
+                println("mutacao: $(mut[2])")
                 println("crossover: $cros")
                 result = Evolutionary.optimize(x -> levy(x), zeros(dimension),
                                                 GA(populationSize = 100,
                                                 selection = sel,
                                                 crossover = cros,
-                                                mutation = mut))
+                                                mutation = mut[1]))
                 println(result)
                 println("===================================================================")
             end
